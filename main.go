73fecf754d4da880fb3da8d6efb0ad8efb685a80
Allan Okoth <allanokoth695@gmail.com>
2023-05-29 01:45:56 +0200

    update main.go

--- main.go
+++ main.go
@@ -22,9 +22,22 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 
 func handleRequests() {
 	http.HandleFunc("/", homePage)
+    // add our articles route and map it to our 
+    // returnAllArticles function like so
+    http.HandleFunc("/articles", returnAllArticles)
 	log.Fatal(http.ListenAndServe(":10000", nil))
 }
 
+func returnAllArticles(w http.ResponseWriter, r *http.Request){
+	fmt.Println("Endpoint Hit: returnAllArticles")
+	json.NewEncoder(w).Encode(Articles)
+}
+
 func main() {
+
+	Articles = []Article{
+		Article{Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+		Article{Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+	}
 	handleRequests()
 }
