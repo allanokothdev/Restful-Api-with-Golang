1549f451a342ee059de89c77db6f448b5fdb3a89
Allan Okoth <allanokoth695@gmail.com>
2023-06-11 00:52:22 +0200

    build(deps): update go.mod and main.go

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module example/restful-api
 
 go 1.20
+
+require github.com/gorilla/mux v1.8.0
--- main.go
+++ main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
-	"encoding/json"
+
+	"github.com/gorilla/mux"
 )
 
 type Article struct {
+	Id    string `json:"Id"`
 	Title string `json:"Title"`
 	Desc string	`json:"desc"`
 	Content string	`json:"content"`
@@ -21,11 +25,74 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 }
 
 func handleRequests() {
-	http.HandleFunc("/", homePage)
-    // add our articles route and map it to our 
-    // returnAllArticles function like so
-    http.HandleFunc("/articles", returnAllArticles)
-	log.Fatal(http.ListenAndServe(":10000", nil))
+	//	Creates a new instance of a mux router
+	myRouter := mux.NewRouter().StrictSlash(true)
+	
+	myRouter.HandleFunc("/", homePage)
+	myRouter.HandleFunc("/articles", returnAllArticles)
+	myRouter.HandleFunc("article", createNewArticle).Methods("POST")
+	myRouter.HandleFunc("/article/{id}", deleteArticle).Methods("DELETE")
+	myRouter.HandleFunc("/article/{id}", updateArticle).Methods("PUT")
+	myRouter.HandleFunc("/article/{id}", returningSingleArticle)
+
+	log.Fatal(http.ListenAndServe(":10000", myRouter))
+}
+
+func returningSingleArticle(w http.ResponseWriter, r *http.Request){
+	vars := mux.Vars(r)
+	key := vars["id"]
+
+	//	Loop over all the articles in the slice
+	//	retrieve the article whose id equals the key we pass in
+	//	return the article encoded as JSON
+
+	for _, article := range Articles {
+		if article.Id == key {
+			json.NewEncoder(w).Encode(article)
+		}
+	}
+
+	fmt.Fprintf(w, "Key: "+ key)
+}
+
+func createNewArticle(w http.ResponseWriter, r *http.Request) {
+	//	get the body of the Post Request
+	//	return the string response containing the request body
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var article Article
+	json.Unmarshal(reqBody, &article)
+	Articles = append(Articles, article)
+	json.NewEncoder(w).Encode(article)
+}
+
+func deleteArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+
+	for index, article := range Articles {
+		if article.Id == id {
+			Articles = append(Articles[:index], Articles[index+1:]...)
+		}
+	}
+}
+
+
+func updateArticle(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	id := vars["id"]
+	reqBody, _ := ioutil.ReadAll(r.Body)
+	var updatedArticle Article
+	json.Unmarshal(reqBody, &updatedArticle)
+
+	for index, article := range Articles {
+		if article.Id == id {
+			article.Content = updatedArticle.Content
+			article.Title = updatedArticle.Title
+			article.Desc = updatedArticle.Desc
+			Articles = append(Articles[:index], article)
+			json.NewEncoder(w).Encode(article)
+		}
+	}
 }
 
 func returnAllArticles(w http.ResponseWriter, r *http.Request){
@@ -34,10 +101,10 @@ func returnAllArticles(w http.ResponseWriter, r *http.Request){
 }
 
 func main() {
-
+	fmt.Println("Rest API c2.0 - Mux Routers")
 	Articles = []Article{
-		Article{Title: "Hello", Desc: "Article Description", Content: "Article Content"},
-		Article{Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
+		Article{Id: "1", Title: "Hello", Desc: "Article Description", Content: "Article Content"},
+		Article{Id: "2", Title: "Hello 2", Desc: "Article Description", Content: "Article Content"},
 	}
 	handleRequests()
 }
